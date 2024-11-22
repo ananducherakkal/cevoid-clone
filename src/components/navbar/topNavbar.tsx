2d9d146ddc0c43c6ab7dfc1b074d5d29e0b77e4f
ananducherakkal <ananducherakkal.dev@gmail.com>
2024-11-22 19:26:55 +0100

    Fix top navbar color change con scroll

--- src/components/navbar/topNavbar.tsx
+++ src/components/navbar/topNavbar.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import HeaderLogo from "./topNavbarLogo";
 import TopNavbarMenu from "./topNavbarMenu";
 import TopNavbarAuthSection from "./topNavbarAuthSection";
 import TopNavbarMobileMenu from "./topNavbarMobileMenu";
+import { cn } from "@/utils/shadcn";
 
 const TopNavbar = () => {
+  const [navbarScroll, setNavbarScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setNavbarScroll(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="w-full fixed top-0 left-0 z-30">
+    <nav
+      className={cn(
+        "w-full fixed top-0 left-0 border-b z-30 bg-gray-10 transition-all duration-200",
+        navbarScroll ? "border-border" : "border-transparent"
+      )}
+    >
       <div className="container-max-w py-4 h-16 lg:h-20 mx-auto flex items-center">
         <HeaderLogo />
         <TopNavbarMenu />
